25570ae3243c4e1c04421f54986567c92c292797
Fernando Pimentel <ferpiolli@gmail.com>
2022-01-27 20:01:39 -0300

    Deve inserir usuario com sucesso

--- test/user.test.js
+++ test/user.test.js
@@ -10,3 +10,12 @@ test('deve listar todos os usuarios', () => {
       expect(res.body[0]).toHaveProperty('name', 'John Doe')
     })
 })
+
+test('Deve inserir usuario com sucesso', () => {
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail: 'walter@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(201)
+      expect(res.body.name).toBe('Walter Mitty')
+    })
+})
