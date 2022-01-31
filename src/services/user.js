701ed17861a8fa23cae705b885ffb1c9fb44170d
Fernando Pimentel <ferpiolli@gmail.com>
2022-01-31 18:56:46 -0300

    Nao deve inserir usuario sem nome

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,10 @@
-module.exports = (app) =>{
+module.exports = (app) => {
   const findAll = () => {
     return app.db('users').select();
   }
 
   const save = (user) => {
+    if (!user.name) return { error: 'Nome é atributo obrigatorio' }
     return app.db('users').insert(user, '*')
   }
   return { findAll, save }
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -9,6 +9,7 @@ test('deve listar todos os usuarios', () => {
     .then((res) => {
       expect(res.status).toBe(200)
       // expect(res.body.lenght).toBeGreaterThan(0)
+      //
     })
 })
 
@@ -19,5 +20,14 @@ test('Deve inserir usuario com sucesso', () => {
     .then((res) => {
       expect(res.status).toBe(201)
       expect(res.body.name).toBe('Walter Mitty')
+    })
+})
+
+test('Não deve inserir usuario sem nome', () => {
+  return request(app).post('/users')
+    .send({ mail: 'fernando@mail.com', passwd: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400)
+      expect(res.body.error).toBe('Nome é atributo obrigatorio')
     })
 })
