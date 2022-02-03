b3d358a1b471fd0f3d85f9d7dde4b26493e4f18a
Fernando Pimentel <ferpiolli@gmail.com>
2022-02-03 14:20:34 -0300

    Nao deve inserir conta sem nome

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -18,6 +18,17 @@ test('Deve inserir uma conta com sucesso', () => {
     })
 })
 
+test('Não deve inserir uma conta sem nome', () => {
+  return request(app).post(MAIN_ROUTE)
+    .send({ user_id: user.id })
+    .then((result) => {
+      expect(result.status).toBe(400)
+      expect(result.body.error).toBe('Nome é um atributo obrigatório')
+    })
+})
+
+test.skip('Não deve inserir uma conta de nome duplicado, para o mesmo usuario', () => {})
+
 test('Deve listar todas as contas', () => {
   return app.db('accounts')
     .insert({ name: 'Acc List', user_id: user.id })
@@ -27,6 +38,9 @@ test('Deve listar todas as contas', () => {
       expect(res.body.length).toBeGreaterThan(0)
     })
 })
+
+test.skip('Deve listar apenas as contas do usuario', () => {})
+
 test('Deve retornar uma conta por Id', () => {
   return app.db('accounts')
     .insert({ name: 'Acc By Id', user_id: user.id }, ['id'])
@@ -37,6 +51,8 @@ test('Deve retornar uma conta por Id', () => {
       expect(res.body.user_id).toBe(user.id)
     })
 })
+
+test.skip('Não deve retornar uma conta de outro usuário', () => {})
 
 test('Deve alterar uma conta', () => {
   return app.db('accounts')
@@ -49,6 +65,8 @@ test('Deve alterar uma conta', () => {
     })
 })
 
+test.skip('Não deve alterar uma conta de outro usuário', () => {})
+
 test('Deve remover uma conta', () => {
   return app.db('accounts')
     .insert({ name: 'Acc To Remove', user_id: user.id }, ['id'])
@@ -57,3 +75,5 @@ test('Deve remover uma conta', () => {
       expect(res.status).toBe(204)
     })
 })
+
+test.skip('Não deve remover uma conta de outro usuário', () => {})
